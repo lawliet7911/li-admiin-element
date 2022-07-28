31c6d5140f8a6addd424903cf42233fd73b22b97
liwenkang <liwenkang@i-xiaoma.com>
2022-07-28 17:53:47 +0800

    feat:init

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+// UI框架
+// import ElementPlus from 'element-plus'
+// import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+// import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
+import 'element-plus/dist/index.css'
+
+createApp(App).mount('#app')
